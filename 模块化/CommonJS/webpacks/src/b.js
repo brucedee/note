a5bc513dab792a428f00cab427248005d985c577
brucedee <email>
2019-06-14 08:20:58 +0800

    ...

--- 模块化/CommonJS/webpacks/src/b.js
+++ 模块化/CommonJS/webpacks/src/b.js
@@ -1,3 +1,4 @@
+import { bs } from './bs';
 export const b1 = 61;
 const b2 = 62;
 let b3 = 63;
@@ -7,3 +8,5 @@ setTimeout(() => {
 export { b2, b3 };
 
 export * from './bb'; // '继承'自bb模块 但不包括bb模块里面的default
+console.log('from b');
+console.log('-----------import命令先于模块内其他语句执行-------------');
---
+++ 模块化/CommonJS/webpacks/src/bs.js
@@ -0,0 +1,6 @@
+console.log('from bs');
+var bs1 = 9999;
+setTimeout(() => {
+  bs1++;
+  console.log(bs1);
+}, 2000);
